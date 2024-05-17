277ee4a54736288619916597a855a585b905d76a
William Quintano <quintanw891@gmail.com>
2024-05-16 22:12:33 -0400

    backup WIP from asus laptop

---
+++ PaperGame/Assets/ObstacleStream.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteAlways]
+public class ObstacleStream : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tilePrefab = null;
+    [SerializeField]
+    [Range(0.1f, 1f)]
+    private float tileToTunnelRatio = 1;
+    [SerializeField]
+    [Range(0f, 5f)]
+    private float speed = 1f;
+
+    private Queue<GameObject> tileQueue;
+    const float TUNNEL_2_SCALE_RATIO = 11;
+
+    void Awake()
+    {
+        tileQueue = new Queue<GameObject>();
+        if (Application.isPlaying)
+        {
+            //Debug.Log("creating in play mode");
+            int numTiles = (int) Math.Ceiling(1f / tileToTunnelRatio) + 1;
+            for (int i=0; i<numTiles; i++)
+            {
+                GameObject tile = Instantiate(tilePrefab, transform, false);
+
+                tile.transform.position =   tile.transform.position +
+                                            (   transform.right * tile.transform.localScale.x *
+                                                TUNNEL_2_SCALE_RATIO * tileToTunnelRatio * i);
+                tile.transform.localScale = tile.transform.localScale * tileToTunnelRatio;
+                tileQueue.Enqueue(tile);
+            }
+        }
+        else if (Application.isEditor)
+        {
+            //Debug.Log("creating in editor");
+            //visualAideList.Add(Instantiate(streamObstaclePrefab, transform, false));
+        }
+    }
+
+    public static GameObject SafeDestroy(GameObject obj)
+    {
+        if (Application.isPlaying)
+        {
+            //Debug.Log("destroying in play mode");
+            Destroy(obj);
+        }
+        else if (Application.isEditor)
+        {
+            //TODO DestroyImmediate below does not work.
+            //     Fix, then restore the instantiate in editor and ExecuteAlways attribute
+
+            //Debug.Log("destroying in editor");
+            DestroyImmediate(obj.gameObject);
+        }
+
+        return null;
+    }
+
+    public static GameObject SafeDestroyGameObject(GameObject gameobject)
+    {
+        if (gameobject != null)
+            SafeDestroy(gameobject);
+        return null;
+    }
+
+    void OnDestroy()
+    {
+        //if (Application.isPlaying)
+        //    Debug.Log("ondestroy called from play mode");
+        //else if (Application.isEditor)
+        //    Debug.Log("ondestroy called from editor");
+
+        //for (int index = 0; index < tileQueue.Count; index++)
+        //{
+        //    tileQueue[index] = SafeDestroyGameObject(tileQueue[index]);
+        //}
+        while (tileQueue.Count > 0)
+        {
+            SafeDestroyGameObject(tileQueue.Dequeue());
+        }
+
+    }
+
+    void Update()
+    {
+        if (Application.isPlaying)
+        {
+            foreach (GameObject tile in tileQueue)
+            {
+                tile.transform.position =   tile.transform.position +
+                                            (   transform.right * -1 * tilePrefab.transform.localScale.x *
+                                                TUNNEL_2_SCALE_RATIO * speed *
+                                                Time.deltaTime);
+            }
+        }
+
+        float tileOffset =  (   tileQueue.Peek().transform.position.x +
+                                (tilePrefab.transform.localScale.x * TUNNEL_2_SCALE_RATIO * tileToTunnelRatio)) -
+                            transform.position.x;
+
+        if ( tileOffset <= 0 )
+        {
+            int numTiles = tileQueue.Count;
+            GameObject tileToRotate = tileQueue.Dequeue();
+            tileToRotate.transform.position =   tileToRotate.transform.position +
+                                                (   transform.right * tilePrefab.transform.localScale.x *
+                                                    TUNNEL_2_SCALE_RATIO * tileToTunnelRatio * numTiles);
+            tileQueue.Enqueue(tileToRotate);
+        } 
+
+    }
+}
--- PaperGame/Assets/Scripts/ObstacleProjector.cs
+++ PaperGame/Assets/Scripts/ObstacleProjector.cs
@@ -6,16 +6,32 @@ public class ObstacleProjector : MonoBehaviour
 {
     private Player player;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        foreach (Transform projectorTransform in transform)
+        {
+            GameObject projectorObject = projectorTransform.gameObject;
+            projectorObject.GetComponent<Projector>().material = new Material(projectorObject.GetComponent<Projector>().material);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //TODO fix below to fade in projector as it approaches player
+        /*foreach (Transform projectorTransform in transform)
+        {
+            GameObject projectorObject = projectorTransform.gameObject;
+            Color prev = projectorObject.GetComponent<Projector>().material.color;
+            //Debug.Log("a is " + projectorObject.GetComponent<Projector>().material.color.a);
+            prev.a = 0f;
+            projectorObject.GetComponent<Projector>().material.color = prev;
+            //projectorObject.GetComponent<Projector>().material.SetColor("_Color", prev);
+        }*/
+        if (transform.position.y >= player.transform.position.y)
+        {
+            Destroy(gameObject);
+        }
+
     }
 
     public void SetPlayer(Player player)
